97985be1beb5b6aa186f323f02334a0d87c8469c
Francis Pimentel <franciserik92@gmail.com>
2021-10-10 12:51:09 -0300

    First commit - project structure creation using Spring Initializr.

---
+++ src/test/java/br/com/francis/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.francis.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
